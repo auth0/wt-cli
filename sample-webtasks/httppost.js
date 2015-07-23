6fb600fad45dd1b0f57b352bf084ed0f804dd47e
Matias Woloski <matiasw@gmail.com>
2015-07-23 19:01:14 -0300

    example

--- sample-webtasks/httppost.js
+++ sample-webtasks/httppost.js
@@ -1,3 +1,8 @@
+/* 
+  wt create httppost.js
+  curl --data foo=bar https://webtask.it.auth0.com/api/run/yours/httppost
+*/
+
 module.exports = 
     function (context, cb) {
         cb(null, 'Hello, world! ' + context.data.foo);
