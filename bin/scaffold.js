f81aadc081f97723238bc7cb685d8b6e6979297e
Milo Mordaunt <milomord@gmail.com>
2015-08-18 14:15:32 -0300

    Use Github API when pulling from Github to save bandwidth

--- bin/scaffold.js
+++ bin/scaffold.js
@@ -1,5 +1,6 @@
 var Cli = require('nested-yargs');
 var Git = require('nodegit');
+var Wreck = require('wreck');
 var Bluebird = require('bluebird');
 var Path = require('path');
 var Rimraf = require('rimraf');
@@ -9,6 +10,16 @@ var JsdParse = require('comment-parser');
 var _ = require('lodash');
 var Create = require('./create');
 
+var ghWreck = Wreck.defaults({
+    baseUrl: 'https://api.github.com/',
+    headers: {
+        'User-Agent': 'wt-cli-scaffold'
+    },
+    json: true,
+});
+
+Bluebird.promisifyAll(ghWreck);
+
 module.exports = Cli.createCommand('scaffold', 'download webtask templates', {
     params: '[webtask] [name]',
     setup: function (yargs) {
@@ -16,7 +27,6 @@ module.exports = Cli.createCommand('scaffold', 'download webtask templates', {
     },
     options: _.assign({}, Create.options.options, {
         repo: {
-            alias: 'r',
             description: 'git repo to use',
             type: 'string',
         },
@@ -24,7 +34,7 @@ module.exports = Cli.createCommand('scaffold', 'download webtask templates', {
     handler: handleScaffold,
 });
 
-var DEFAULT_REPO_URL = 'https://github.com/bananaoomarang/wt-cli';
+var DEFAULT_REPO_NAME = 'auth0/wt-cli';
 var DEFAULT_REPO_SAMPLE_DIR = 'sample-webtasks/';
 
 function handleScaffold(argv) {
@@ -37,26 +47,10 @@ function handleScaffold(argv) {
 
     // If there's no slash or repo we assume they want to pull from the samples
     if(!/\//.test(argv.params.webtask) && !argv.repo)
-        return getCommit(DEFAULT_REPO_URL, 'master')
-            .then(function (commit) {
-                return scaffoldFrom(argv, commit, DEFAULT_REPO_SAMPLE_DIR + argv.params.webtask)
-                    .catch(function (e) {
-                        if(e.message.match('does not exist')) {
-                            console.error('Requested webtask %s does not exist.\n'.red, Path.basename(argv.params.webtask));
-
-                            return listScaffolds(commit);
-                        }
-
-                        throw e;
-                    });
-            })
-            .then(cleanup)
-            .catch(catchErr);
-
-    var ghRepoPath = argv.params.webtask
-            .split('/')
-            .slice(0, 2)
-            .join('/');
+        return getHead(DEFAULT_REPO_NAME, 'master')
+            .then(getTree)
+            .then(getBlob.bind(null, Path.join(DEFAULT_REPO_SAMPLE_DIR, argv.params.webtask)))
+            .then(scaffold);
 
     var wtPath = argv.repo ?
         argv.params.webtask :
@@ -65,14 +59,47 @@ function handleScaffold(argv) {
             .slice(2)
             .join('/');
 
-    var url = argv.repo || ('https://github.com/' + ghRepoPath);
+    if(argv.repo)
+        return getCommit(argv.repo, 'master')
+            .then(function (commit) {
+                return commit
+                    .getEntry(wtPath)
+                    .then(function (entry) {
+                        return entry.getBlob()
+                    })
+                    .then(function (blob) {
+                        return scaffold(blob.toString('utf8'));
+                    });
+            })
+            .then(cleanup)
+            .catch(catchErr);
 
-    return getCommit(url, 'master')
-        .then(function (commit) {
-            return scaffoldFrom(argv, commit, wtPath)
-        })
-        .then(cleanup)
-        .catch(catchErr);
+    var ghRepo = argv.params.webtask
+            .split('/')
+            .slice(0, 2)
+            .join('/');
+
+    return getHead(ghRepo, 'master')
+        .then(getTree)
+        .then(getBlob.bind(null, wtPath))
+        .then(scaffold);
+
+    function scaffold(str) {
+        var filename = argv.params.name ? (argv.params.name + '.js') : 'webtask.js';
+
+        return WriteFile(filename, str)
+            .then(function () {
+                argv.params.file_or_url = filename;
+                argv.output = 'none';
+                argv.name = argv.params.name || Path.basename(filename, '.js');
+
+                return Create.options.handler(argv);
+            })
+            .then(function (result) {
+                console.log('Scaffold written to '.blue, filename.bold.green);
+                console.log('Scaffold deployed to'.blue, result.named_webtask_url.bold.green)
+            });
+    }
 
     function catchErr(e) {
         if(e.message === '\'.tmp\' exists and is not an empty directory') {
@@ -86,15 +113,11 @@ function handleScaffold(argv) {
     }
 }
 
-function listScaffolds(commit) {
-    var promise = commit ?
-        commit.getTree() :
-        getCommit(DEFAULT_REPO_URL, 'master')
-            .then(function (commit) {
-                return commit.getTree();
-            })
-
-    return promise 
+function listScaffolds() {
+    return getCommit('https://github.com/' + DEFAULT_REPO_NAME, 'master')
+        .then(function (commit) {
+            return commit.getTree();
+        })
         .then(function (tree) {
             return tree.getEntry(DEFAULT_REPO_SAMPLE_DIR);
         })
@@ -129,41 +152,63 @@ function listScaffolds(commit) {
                 });
         })
         .then(function (scaffolds) {
-            console.log('Available webtasks:'.green.bold);
+            console.log('Available webtasks:\n'.green.bold);
 
             scaffolds
                 .forEach(function (scaffold) {
-                    console.log(scaffold.name.bold.white + ':', scaffold.description.grey);
+                    console.log('\t' + scaffold.name.bold.white + ':', scaffold.description.grey);
                 });
-        });
-}
 
-function scaffoldFrom(argv, commit, path) {
-    var filename = argv.params.name ? (argv.params.name + '.js') : 'webtask.js';
-
-    return commit
-        .getEntry(path)
-        .then(function (entry) {
-            return entry.getBlob();
-        })
-        .then(function (blob) {
-            return WriteFile(filename, blob.toString());
-        })
-        .then(function () {
-            argv.params.file_or_url = filename;
-            argv.output = 'none';
-            argv.name = argv.params.name || Path.basename(filename, '.js');
-
-            return Create.options.handler(argv);
-        })
-        .then(function (result) {
-            console.log('Scaffold written to '.blue, filename.bold.green);
-            console.log('Scaffold deployed to'.blue, result.named_webtask_url.bold.green)
+            console.log('');
         });
 }
 
 function cleanup() {
     Rimraf.sync('.tmp');
+}
+
+function getHead(reponame, headname) {
+    var url = Path.join('repos', reponame, 'git/refs/heads', headname);
+
+    return ghWreck
+        .getAsync(url)
+        .spread(function (res, body) {
+            if(body.object)
+                return body.object;
+
+            throw 'Could not get head: ' + body.message;
+        });
+}
+
+function getTree(commit) {
+    return ghWreck
+        .getAsync(commit.url)
+        .spread(function (res, body) {
+            return ghWreck
+                .getAsync(body.tree.url + '?recursive=1')
+                .spread(function (res, body) {
+                    return body.tree;
+                });
+        });
+}
+
+function getBlob(path, tree) {
+    var matches = tree
+        .filter(function (twig) {
+            return twig.path.match(path);
+        });
+
+    if(!matches.length)
+        throw 'File not found';
+
+    if(matches.length > 1)
+        throw 'Attempted to scaffold from directory, please supply path to file';
+
+    return ghWreck
+        .getAsync(matches[0].url)
+        .spread(function (res, body) {
+            return (new Buffer(body.content, 'base64').toString('utf8'));
+        });
 }
 
 function getCommit(repo, branch) {
