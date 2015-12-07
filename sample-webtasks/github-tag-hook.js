88c320e546d4d5592a8e720091e018b787ed190d
Geoffrey Goodman <ggoodman@gmail.com>
2015-12-07 16:29:04 -0500

    Add instructions for webhook setup

--- sample-webtasks/github-tag-hook.js
+++ sample-webtasks/github-tag-hook.js
@@ -12,6 +12,14 @@ var REF = 'refs/heads/master';
  * A webtask that can be used as a Github webhook to automatically tag new
  * versions based on changes to the file `package.json`.
  * 
+ * Installation instructions:
+ * 1. Install the webtask cli: `npm install -g wt-cli`
+ * 2. Create a webtask profile: `wt init`
+ * 3. Create a Github API token with `repo` access from: https://github.com/settings/tokens/new
+ * 4. Generate the webhook url, substituting <YOUR_TOKEN> with the one from step #3: `wt create --name auto_tag --secret GITHUB_TOKEN=<YOUR_TOKEN> --prod https://raw.githubusercontent.com/auth0/wt-cli/master/sample-webtasks/github-tag-hook.js`
+ * 5. Install the webhook with the default settings on your repo by subsituting <USERNAME> and <REPO>, at: https://github.com/<USERNAME>/<REPO>/settings/hooks/new
+ * 6. Optionally inspect any errors using the cli: `wt logs`
+ * 
  * @webtask_option pb 1 - This webtask requires that the body automatically be parsed
  * @webtask_secret GITHUB_TOKEN - A Github access token
  */
