840a9ed121d540a56acee791c63da26597c3597f
Geoffrey Goodman <ggoodman@gmail.com>
2016-02-25 16:06:10 -0500

    wt create / wt cron schedule: Add --show-token. Fixes #44

--- bin/create.js
+++ bin/create.js
@@ -20,6 +20,11 @@ module.exports = Cli.createCommand('create', {
                 choices: ['json'],
                 type: 'string',
             },
+            'show-token': {
+                description: 'Show tokens (hidden by default)',
+                dest: 'showToken',
+                type: 'boolean',
+            },
         },
         'Webtask creation': {
             'secret': {
@@ -101,13 +106,22 @@ function handleCreate(args) {
     var profile = args.profile;
     var createWebtask = WebtaskCreator(args, {
         onGeneration: onGeneration,
+        onError: onError,
     });
+    var logger = args.watch
+        ?   Logs.createLogStream(profile)
+        :   console;
     var log = args.watch
-        ?   _.bindKey(Logs.createLogStream(profile), 'info')
-        :   _.bindKey(console, 'log');
+        ?   _.bindKey(logger, 'info')
+        :   _.bindKey(logger, 'log');
+    var logError = _.bindKey(logger, 'error');
 
     return createWebtask(profile);
     
+    
+    function onError(build) {
+        formatError(build);
+    }
     
     function onGeneration(build) {
         var parsedUrl = Url.parse(build.webtask.url, true);
@@ -118,15 +132,56 @@ function handleCreate(args) {
         
         var url = Url.format(parsedUrl);
         
-        args.watch
-            ?   log({ generation: build.generation, container: build.webtask.container, token: build.webtask.token }, 'Webtask created: %s', url)
-            :   args.output === 'json'
-                ?   log({
-                        url: url,
-                        token: build.webtask.token,
-                        name: build.webtask.claims.jtn,
-                    })
-                :   log(Chalk.green('Webtask token created') + '\n\n%s\n\nYou can access your webtask at the following url:\n\n%s', Chalk.gray(build.webtask.token), Chalk.bold(url));
+        formatOutput(build, url);
+    }
+    
+    function formatError(build, url) {
+        var output;
+        
+        if (args.watch) {
+            output = { generation: build.generation };
+            
+            _.forEach(build.stats.errors, function (error) {
+                logError(output, 'Bundling failed: %s', error);
+            });
+        } else if (args.output === 'json') {
+            output = { url: url, name: build.webtask.claims.jtn, container: build.webtas.container };
+            
+            if (args.showToken) {
+                output.token = build.webtask.token;
+            }
+            
+            logError(JSON.stringify(build.stats.errors, null, 2));
+        } else {
+            logError(Chalk.red('Bundling failed failed'));
+            build.stats.errors.forEach(logError);
+        }
+    }
+    
+    function formatOutput(build, url) {
+        var output;
+        
+        if (args.watch) {
+            output = { generation: build.generation, container: build.webtask.container };
+            
+            if (args.showToken) {
+                output.token = build.webtask.token;
+            }
+            
+            log(output, 'Webtask created: %s', url);
+        } else if (args.output === 'json') {
+            output = { url: url, name: build.webtask.claims.jtn, container: build.webtas.container };
+            
+            if (args.showToken) {
+                output.token = build.webtask.token;
+            }
+            
+            log(JSON.stringify(output, null, 2));
+        } else if (args.showToken) {
+            log(Chalk.green('Webtask token created') + '\n\n%s\n\nYou can access your webtask at the following url:\n\n%s', Chalk.gray(build.webtask.token), Chalk.bold(url));
+        } else {
+            log(Chalk.green('Webtask created') + '\n\nYou can access your webtask at the following url:\n\n%s', Chalk.bold(url));
+        }
     }
 }
 
