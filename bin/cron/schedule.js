3b01a1b381fdaf36c91ca9bfbd2489b0ce641347
Geoffrey Goodman <ggoodman@gmail.com>
2016-02-25 17:16:10 -0500

    wt cron schedule: Add support for human-readable intervals. Fixes #48

--- bin/cron/schedule.js
+++ bin/cron/schedule.js
@@ -6,6 +6,51 @@ var WebtaskCreator = require('../../lib/webtaskCreator');
 var _ = require('lodash');
 
 
+var intervals = {
+    minutes: {
+        abbrev: ['m', 'min', 'mins', 'minute', 'minutes'],
+        values: [ 1, 2, 3, 4, 5, 6, 10, 15, 20, 30, 60 ],
+        encode: function (frequencyValue) {
+            var now = new Date();
+            var cron = ['*', '*', '*', '*', '*'];
+            var curr = now.getMinutes();
+            var mod = curr % frequencyValue;
+
+            cron[0] = frequencyValue === 60
+                ?   curr
+                :   mod > 0
+                    ?  (curr % frequencyValue) + '-' + (59) + '/' + (frequencyValue)
+                    :   '*/' + (frequencyValue);
+            
+            return cron.join(' ');
+        },
+    },
+    hours: {
+        abbrev: ['h', 'hour', 'hours' ],
+        values: [ 1, 2, 3, 4, 6, 8, 12, 24],
+        encode: function (frequencyValue) {
+            var now = new Date();
+            var cron = [now.getMinutes(), '*', '*', '*', '*'];
+            var curr = now.getHours();
+            var mod = curr % frequencyValue;
+
+            cron[1] = frequencyValue === 24
+                ?   curr
+                :   mod > 0
+                    ?  (curr % frequencyValue) + '-' + (23) + '/' + (frequencyValue)
+                    :   '*/' + (frequencyValue);
+            
+            return cron.join(' ');
+        },
+    },
+    days: {
+        abbrev: ['d', 'day', 'days'],
+        values: [ 1 ],
+        encode: function () { return intervals.hours.encode(24); },
+    },
+};
+
+
 var createCommand = require('../create');
 
 
@@ -19,11 +64,18 @@ module.exports = Cli.createCommand('schedule', {
     }),
     params: _.extend({}, {
         'schedule': {
-            description: 'The cron-formatted schedule (see: http://crontab.guru/)',
+            description: 'Either a cron-formatted schedule (see: http://crontab.guru/) or an interval of hours ("h") or minutes ("m"). Note that not all intervals are possible.',
             type: 'string',
             required: true,
         },
     }, createCommand.params),
+    epilog: [
+        'Examples:',
+        '1. Create a webtask that runs every five minutes:',
+        '  $ wt cron schedule 5m ./sample-webtasks/hello-world.js',
+        '2. Create a webtask that runs every Tuesday at 09:05:',
+        '  $ wt cron schedule "5 9 * * 2" ./sample-webtasks/hello-world.js',
+    ].join('\n'),
     handler: handleCronSchedule,
 });
 
@@ -34,6 +86,36 @@ function handleCronSchedule(args) {
     var profile = args.profile;
     
     args = ValidateCreateArgs(args);
+    
+    var schedule = args.schedule;
+    
+    if (schedule.split(' ').length !== 5) {
+        var minutesRx = new RegExp('^\s*([0-9]{1,2})\s*(' + intervals.minutes.abbrev.join('|') + ')\s*$', 'i');
+        var hoursRx = new RegExp('^\s*([0-9]{1,2})\s*(' + intervals.hours.abbrev.join('|') + ')\s*$', 'i');
+        var daysRx = new RegExp('^\s*([0-9]{1,2})\s*(' + intervals.days.abbrev.join('|') + ')\s*$', 'i');
+        var frequencyValue;
+        var type;
+        var matches;
+        
+        if ((matches = schedule.match(minutesRx))) {
+            type = intervals.minutes;
+            frequencyValue = parseInt(matches[1], 10);
+        } else if ((matches = schedule.match(hoursRx))) {
+            type = intervals.hours;
+            frequencyValue = parseInt(matches[1], 10);
+        } else if ((matches = schedule.match(daysRx))) {
+            type = intervals.days;
+            frequencyValue = parseInt(matches[1], 10);
+        } else {
+            throw new Cli.error.invalid('The schedule `' + schedule + '` is not valid.');
+        }
+        
+        if (type.values.indexOf(frequencyValue) === -1) {
+            throw new Cli.error.invalid('For intervals in ' + type.abbrev[type.abbrev.length - 1] + ', the following intervals are supported: ' + type.values.join(', '));
+        }
+        
+        schedule = type.encode(frequencyValue);
+    }
     
     var createWebtask = WebtaskCreator(args, {
         onGeneration: onGeneration,
