0cc0e5b35f7484b76367014078829d16ea5fb62b
Geoffrey Goodman <ggoodman@gmail.com>
2016-03-15 14:21:28 -0400

    wt update: Implement webtask update

--- bin/create.js
+++ bin/create.js
@@ -1,10 +1,7 @@
 var Chalk = require('chalk');
 var Cli = require('structured-cli');
-var Logs = require('../lib/logs');
+var CreateWebtask = require('../lib/createWebtask');
 var ValidateCreateArgs = require('../lib/validateCreateArgs');
-var WebtaskCreator = require('../lib/webtaskCreator');
-var Url = require('url');
-var _ = require('lodash');
 
 
 module.exports = Cli.createCommand('create', {
@@ -105,86 +102,7 @@ module.exports = Cli.createCommand('create', {
 function handleCreate(args) {
     args = ValidateCreateArgs(args);
     
-    var profile = args.profile;
-    var createWebtask = WebtaskCreator(args, {
-        onGeneration: onGeneration,
-        onError: onError,
-    });
-    var logger = args.watch
-        ?   Logs.createLogStream(profile)
-        :   console;
-    var log = args.watch
-        ?   _.bindKey(logger, 'info')
-        :   _.bindKey(logger, 'log');
-    var logError = _.bindKey(logger, 'error');
-
-    return createWebtask(profile);
-    
-    
-    function onError(build) {
-        formatError(build);
-    }
-    
-    function onGeneration(build) {
-        var parsedUrl = Url.parse(build.webtask.url, true);
-        
-        if (!args.prod) {
-            parsedUrl.query.webtask_no_cache = 1;
-        }
-        
-        var url = Url.format(parsedUrl);
-        
-        formatOutput(build, url);
-    }
-    
-    function formatError(build, url) {
-        var output;
-        
-        if (args.watch) {
-            output = { generation: build.generation };
-            
-            _.forEach(build.stats.errors, function (error) {
-                logError(output, 'Bundling failed: %s', error);
-            });
-        } else if (args.output === 'json') {
-            output = { url: url, name: build.webtask.claims.jtn, container: build.webtas.container };
-            
-            if (args.showToken) {
-                output.token = build.webtask.token;
-            }
-            
-            logError(JSON.stringify(build.stats.errors, null, 2));
-        } else {
-            logError(Chalk.red('Bundling failed failed'));
-            build.stats.errors.forEach(logError);
-        }
-    }
-    
-    function formatOutput(build, url) {
-        var output;
-        
-        if (args.watch) {
-            output = { generation: build.generation, container: build.webtask.container };
-            
-            if (args.showToken) {
-                output.token = build.webtask.token;
-            }
-            
-            log(output, 'Webtask created: %s', url);
-        } else if (args.output === 'json') {
-            output = { url: url, name: build.webtask.claims.jtn, container: build.webtask.container };
-            
-            if (args.showToken) {
-                output.token = build.webtask.token;
-            }
-            
-            log(JSON.stringify(output, null, 2));
-        } else if (args.showToken) {
-            log(Chalk.green('Webtask token created') + '\n\n%s\n\nYou can access your webtask at the following url:\n\n%s', Chalk.gray(build.webtask.token), Chalk.bold(url));
-        } else {
-            log(Chalk.green('Webtask created') + '\n\nYou can access your webtask at the following url:\n\n%s', Chalk.bold(url));
-        }
-    }
+    return CreateWebtask(args, { action: 'created' });
 }
 
 
---
+++ bin/update.js
@@ -0,0 +1,86 @@
+var Cli = require('structured-cli');
+var CreateWebtask = require('../lib/createWebtask');
+var ValidateCreateArgs = require('../lib/validateCreateArgs');
+
+
+module.exports = Cli.createCommand('update', {
+    description: 'Update the code of a named webtask',
+    plugins: [
+        require('./_plugins/profile'),
+    ],
+    optionGroups: {
+        'Output options': {
+            output: {
+                alias: 'o',
+                description: 'Set the output format',
+                choices: ['json'],
+                type: 'string',
+            },
+            'show-token': {
+                description: 'Show tokens (hidden by default)',
+                dest: 'showToken',
+                type: 'boolean',
+            },
+        },
+        'Webtask creation': {
+            'watch': {
+                alias: 'w',
+                description: 'Automatically watch and reprovision the webtask on local file changes. This will also subscribe you to logs as if you had done `wt logs` to provide an intuitive development experience without requiring multiple active terminals.',
+                type: 'boolean',
+            },
+            'bundle': {
+                alias: 'b',
+                description: 'Use `webtask-bundle` to bundle your code into a single file. This tool can compile ES2015 (ES6) code via Babel as well as packaging up a webtask composed of multiple files into a single file. The tool will scan your package.json for dependencies and will automatically bundle those that are not available on the webtask platform. Enabling --bundle-loose will prevent this check from doing strict semver range comparisons on dependencies.',
+                type: 'boolean',
+            },
+            'bundle-strict': {
+                description: 'Enforce strict semver matching for bundling with `webtask-bundle`',
+                dest: 'loose',
+                action: 'storeFalse',
+                defaultValue: true,
+                type: 'boolean',
+            },
+            'capture': {
+                description: 'Download and use the current code indicated by `url`. When you are developing a webtask whose code is remotely hosted, this option will automatically download the remote code before creating the webtask. This means that the webtask will continue to run even if the remote url becomes unavailable.',
+                type: 'boolean',
+            },
+        },
+    },
+    params: {
+        'name': {
+            description: 'The name of the webtask to update.',
+            type: 'string',
+            required: true,
+        },
+        'file_or_url': {
+            description: 'Path or URL of the webtask\'s code. When not specified, code will be read from STDIN.',
+            type: 'string',
+        },
+    },
+    handler: handleUpdate,
+});
+
+
+// Command handler
+
+function handleUpdate(args) {
+    args = ValidateCreateArgs(args);
+    
+    var profile = args.profile;
+    
+    return profile.inspectWebtask({ name: args.name, decrypt: true })
+        .then(onClaims);
+        
+    
+    function onClaims(claims) {
+        // Set the user-defined options from the inspected webtask's claims
+        args.merge = claims.mb;
+        args.parse = claims.pb;
+        args.secrets = claims.ectx;
+        
+        // Defer to the functionality of the create command
+        return CreateWebtask(args, { action: 'updated' });
+    }
+}
+
+
