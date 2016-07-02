88e1b6a4ea8f5839500cde2716f9b6c2af2c8ddd
Greg Johns <greg.johns@gmail.com>
2016-07-02 09:34:51 -0500

    updated to const

--- sample-webtasks/es6-template-literals.js
+++ sample-webtasks/es6-template-literals.js
@@ -1,7 +1,5 @@
-"use latest";
-
 module.exports = function (context, req, res) {
-    let templateLiteral = `<h1>Howdy, ${context.data.name}!</h1>`;
+    const templateLiteral = `<h1>Howdy, ${context.data.name}!</h1>`;
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(templateLiteral);
