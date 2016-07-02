bb000020990547503d257daa7c01f80c5fb2d707
Carlos Johnson <carlos.johnson@pipedrive.com>
2016-07-03 02:37:57 +0300

    mv: inside (rename) and cross-container move

--- bin/mv.js
+++ bin/mv.js
@@ -1,6 +1,7 @@
+var url = require('url');
+var _ = require('lodash');
 var Chalk = require('chalk');
 var Cli = require('structured-cli');
-var _ = require('lodash');
 
 module.exports = Cli.createCommand('mv', {
     description: 'Move a named webtask',
@@ -35,14 +36,75 @@ module.exports = Cli.createCommand('mv', {
 });
 
 function handleWebtaskMove(args) {
+    var options = _(args).pick(['targetContainer', 'targetProfile']).omitBy(_.isNull).value();
+    var targetName = args.target || args.source;
 
-    /**
-     * @type {WebtaskProfile}
-     */
-    moveWebtask(args.source, args.target || args.source, _.pick(args, ['targetContainer', 'targetProfile']))
+    return moveWebtask(args.profile, args.source, {
+        profile: options.targetProfile,
+        container: options.targetContainer,
+        name: targetName
+    }).then(function() {
+        console.log(Chalk.green('Moved webtask: %s'), Chalk.bold(targetName));
+    });
 }
 
-function moveWebtask(source, target, options) {
-    // TODO: 
-    console.log(Chalk.yellow('moveWebtask:') + ' source=%j, target=%j, options=%j', source, target, options);
+// TODO: validate new and remove original webtask
+function moveWebtask(profile, name, target) {
+    var source = {
+        name: name,
+        container: profile.container,
+        profile: profile.name
+    };
+
+    if (equal(source, target)) {
+        throw Cli.error.invalid('Webtasks are identical. Use a different target name, container or profile.');
+    }
+
+    return read(profile, name)
+        .then(function(webtask) {
+            return copy(profile, webtask, target);
+        });
+}
+
+// TODO: harden
+function equal(source, target) {
+    return source.name == target.name && !target.container && !target.profile;
+}
+
+function read(profile, name) {
+    return profile.inspectWebtask({
+            name: name,
+            decrypt: true,
+            fetch_code: true
+        })
+        .catch(function(err) {
+            if (err.statusCode === 404) {
+                throw Cli.error.notFound('No such webtask: ' + Chalk.bold(name));
+            }
+            throw err;
+        })
+        .then(function(webtask) {
+            return webtask;
+        });
+}
+
+// TODO: copy between profiles
+// TODO: copy cronjobs schedules
+function copy(profile, webtask, target) {
+    var hasInlineCode = url.parse(webtask.url).protocol === 'webtask';
+    var claims = _(webtask).omit(['jti', 'iat', 'ca']).value();
+
+    if (hasInlineCode) {
+        delete claims.url;
+    } else {
+        delete claims.code;
+    }
+
+    claims.jtn = target.name || claims.jtn;
+    claims.ten = target.container || claims.ten;
+
+    return profile.createRaw(claims)
+        .catch(function(err) {
+            throw Cli.error.cancelled('Failed to create webtask. ' + err);
+        });
 }
