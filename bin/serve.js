03949da1c2a03c74e8cf719231edf23febf3db6d
Glenn Block <glenn.block@gmail.com>
2016-11-12 18:15:20 -0800

    Adding local debugging

--- bin/serve.js
+++ bin/serve.js
@@ -6,8 +6,9 @@ const Chalk = require('chalk');
 const Cli = require('structured-cli');
 const Path = require('path');
 const Runtime = require('webtask-runtime');
+const spawn = require('child_process').spawn;
 const _ = require('lodash');
-
+var i = null;
 
 module.exports = Cli.createCommand('serve', {
     description: 'Run a webtask as a local http server',
@@ -24,6 +25,14 @@ module.exports = Cli.createCommand('serve', {
                 type: 'string',
                 defaultValue: '0.0.0.0',
             },
+        },
+        'Execution options': {
+            'debugtask': {
+                alias: 'd',
+                description: 'Debug your task locally. "devtool" requires installing the devtool cli (npm install devtool -g)',
+                choices: ['devtool', 'node'],
+                dest: 'debugtask'
+            }
         },
         'Webtask creation': {
             'secret': {
@@ -78,6 +87,10 @@ function handleWebtaskServe(args) {
     parseKeyValList(args, 'secrets');
     parseKeyValList(args, 'params');
 
+    if (args.debugtask != undefined) {
+        return handleDebug();
+    } 
+    
     return Bluebird.using(createServer(), server => {
         return server.listenAsync(args.port, args.hostname)
             .tap(() => {
@@ -92,6 +105,56 @@ function handleWebtaskServe(args) {
     })
         .timeout(1000 * 60 * 30);
     
+    
+    function handleDebug() {
+        process = require('process');
+        
+        i = process.argv.findIndex((item) => {return item.startsWith("--debugtask")});
+        i = i > -1 ? i : process.argv.findIndex((i) => {return i.startsWith("-d=")});
+
+        if (args.debugtask == "node") {
+            return new Bluebird(debugNode);
+        }
+        else if (args.debugtask == "devtool") {
+            return new Bluebird(debugDevtool);
+        }
+    }
+    
+    function debugNode(resolve, reject) {
+        var newArgs = ['--debug'].concat(process.argv.slice(1, i)); 
+
+        if (process.argv.length >= i-1) 
+        {
+            var newArgs = newArgs.concat(process.argv.slice(i+1, process.argv.length));
+        }
+        spawnProcess(process.execPath, newArgs, resolve);
+    }
+
+    function debugDevtool(resolve, reject) {
+        var newArgs = process.argv.slice(1, i); 
+
+        if (process.argv.length >= i-1) 
+        {
+            var newArgs = newArgs.concat(process.argv.slice(i+1, process.argv.length));
+        }
+        spawnProcess('devtool', newArgs, resolve);
+    }
+
+    function spawnProcess(launcher, args, resolve) {
+        var node = spawn(launcher, args);
+        
+        node.stdout.on('data', (data) => {
+            console.log(`${data}`);
+        });
+
+        node.stderr.on('data', (data) => {
+            console.error(`${data}`);
+        });
+
+        node.on('close', (code) => {
+            resolve();
+        });
+    }
 
     function createServer() {
         const promise$ = new Bluebird((resolve, reject) => {
