404d52bd97a1aec38b93b9b182f628dda54eb586
Shahjada <shahjada.talukdar@gmail.com>
2016-11-15 16:25:54 +0600

    Express with Twit

---
+++ sample-webtasks/auth0-tweets.js
@@ -0,0 +1,26 @@
+var Express = require('express');
+var Twitter = require('twit');
+var Webtask = require('webtask-tools');
+
+var app = Express();
+
+app.use(require('body-parser').json());
+
+var API_KEY = "fekTlACzGXVbJxEcLci9Zxz6K";
+var API_SECRET = "dG4hOTSJWPdHbR5xcDfo9T8QgWS3aXhjK44ycoec7P264wqGQk";
+var ACCESS_TOKEN = "4026347956-4s00pRs6E9H2vF3VxwiENljUG1ntv0ChR2pQ9r9";
+var ACCESS_TOKEN_SECRET = "TCGKQwvZtgtWRd1bejbGoLHapv1xA740D4W2iisanQKJA";
+
+var secret = {
+  consumer_key: API_KEY,
+  consumer_secret: API_SECRET,
+  access_token: ACCESS_TOKEN,
+  access_token_secret: ACCESS_TOKEN_SECRET
+};
+
+var twitter = new Twitter(secret);
+
+
+// expose this express app as a webtask-compatible function
+module.exports = Webtask.fromExpress(app);
+
