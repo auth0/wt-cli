f3c34b01d74794eb07c9d88b4fdaaeb9171513bb
Tomasz Janczuk <tomasz@janczuk.org>
2017-01-24 18:16:58 -0800

    define compilers for extensions

--- bin/auth0_extensions.js
+++ bin/auth0_extensions.js
@@ -1,6 +1,7 @@
 module.exports = {
     'pre-user-registration': {
         description: 'The pre-user-registration extension allows custom code to prevent creation of an appliction user or to add custom app_metadata or user_metadata to a newly created user.',
+        compiler: 'auth0-ext-compilers/pre-user-registration',
         template: f(function () {/*
 module.exports = function (user, context, cb) {
   // call the callback with an error to signal failure
@@ -16,6 +17,7 @@ module.exports = function (user, context, cb) {
     },
     'post-user-registration': {
         description: 'The post-user-registration extension allows custom code to implement custom actions in response to creation of a new application user in the database.',
+        compiler: 'auth0-ext-compilers/post-user-registration',
         template: f(function () {/*
 module.exports = function (user, context, cb) {
   // Send message to Slack etc.
@@ -24,6 +26,7 @@ module.exports = function (user, context, cb) {
     },
     'client-credentials-exchange': {
         description: 'The client-credentials-exchange extension allows custom code to modify the scopes and add custom claims to the tokens issued from the POST /oauth/token Auth0 API.',
+        compiler: 'auth0-ext-compilers/client-credentials-exchange',
         template: f(function () {/*
 module.exports = function (client, scope, audience, context, cb) {
   // call the callback with an error to signal authorization failure
@@ -34,6 +37,7 @@ module.exports = function (client, scope, audience, context, cb) {
     },
     'password-exchange': {
         description: 'The password-exchange extension allows custom code to modify the scopes and add custom claims to the tokens issued from the POST /oauth/token Auth0 API using grant_type=password.',
+        compiler: 'auth0-ext-compilers/password-exchange',
         template: f(function () {/*
 module.exports = function (user, client, scope, audience, context, cb) {
   // call the callback with an error to signal authorization failure
