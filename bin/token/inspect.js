48bc6c32c2f9630b1f2360dd1c5209ae4b2c31be
Geoffrey Goodman <ggoodman@gmail.com>
2017-04-05 11:12:55 -0700

    (feat) wt inspect: show metadata

--- bin/token/inspect.js
+++ bin/token/inspect.js
@@ -45,25 +45,25 @@ module.exports = Cli.createCommand('inspect', {
 function handleTokenInspect(args) {
     var profile = args.profile;
     var claims;
-    
+
     try {
         claims = Decode(args.subject);
     } catch (__) { }
-    
+
     var inspection$ = claims
-        ?   profile.inspectToken({ token: args.subject, decrypt: args.decrypt, fetch_code: args.fetchCode })
-        :   profile.inspectWebtask({ name: args.subject, decrypt: args.decrypt, fetch_code: args.fetchCode });
-    
+        ?   profile.inspectToken({ token: args.subject, decrypt: args.decrypt, fetch_code: args.fetchCode, meta: 1 })
+        :   profile.inspectWebtask({ name: args.subject, decrypt: args.decrypt, fetch_code: args.fetchCode, meta: 1 });
+
     return inspection$
         .catch(function (err) {
             if (err.statusCode >= 500) throw Cli.error.serverError(err.message);
             if (err.statusCode >= 400) throw Cli.error.badRequest(err.message);
-            
+
             throw err;
         })
         .then(onTokenData);
-        
-    
+
+
     function onTokenData(data) {
         if (args.output === 'json') {
             console.log(JSON.stringify(data, null, 2));
