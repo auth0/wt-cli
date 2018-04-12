947191ef9297162ffe8f2c82b89cc9f708144124
Les Zychowski <les.zychowski@auth0.com>
2018-04-12 11:04:40 -0500

    Adding support for securing webtask execution

--- bin/update.js
+++ bin/update.js
@@ -63,6 +63,14 @@ module.exports = Cli.createCommand('update', {
                 description: 'Download and use the current code indicated by `url`. When you are developing a webtask whose code is remotely hosted, this option will automatically download the remote code before creating the webtask. This means that the webtask will continue to run even if the remote url becomes unavailable.',
                 type: 'boolean',
             },
+            'secure': {
+                description: 'Allow securing of the execution of a webtask.  See https://www.npmjs.com/package/@webtask/jwt-middleware for more details.',
+                type: 'boolean'
+            },
+            'execution-scope': {
+                description: 'Set the wt-execution-scope metadata property to the name of a custom scope that can be used for authorization of webtask execution. Requires --secure flag to be set.',
+                type: 'string'
+            }
         },
     },
     params: {
